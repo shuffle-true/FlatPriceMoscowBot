33d279e2a86d16e53801b7924df998a9d20a9fe6
LilPomidorLil <rozhinthe@ya.ru>
2022-05-24 17:15:53 +0300

    небольшие изменения

--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -1,10 +1,7 @@
 import logging
 
 from aiogram import Dispatcher
-
 from data.config import admins
-
-from data.all_config import TOKEN
 
 async def on_startup_notify(dp: Dispatcher):
     for admin in admins:
@@ -13,15 +10,3 @@ async def on_startup_notify(dp: Dispatcher):
 
         except Exception as err:
             logging.exception(err)
-
-def check_():
-    # if TOKEN == "2145614320:AAEEkcgJRryvRVPhbVUdOWPAvM7iZwAV718":
-    #     raise AttributeError(
-    #         "Необходимо изменить токен бота. Укажите его в директории программы ../data/all_config.py\n"
-    #                          "Указать в режиме для чтения!"
-    #     )
-
-    if len(admins) == 0:
-        raise ValueError(
-            "Не указан ID админа бота. Укажите его в директории программы ../data/config.py --- admins"
-        )
