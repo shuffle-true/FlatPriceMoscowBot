137a58010b91dcfd8d87011b639ff2267935b6bb
LilPomidorLil <rozhinthe@ya.ru>
2022-05-24 17:14:42 +0300

    небольшие изменения

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from utils.set_bot_commands import set_default_commands
-from utils.notify_admins import check_
 
 async def on_startup(dp):
     import filters
@@ -15,5 +14,4 @@ if __name__ == '__main__':
     from aiogram import executor
     from handlers import dp
 
-    check_()
     executor.start_polling(dp, on_startup=on_startup)
